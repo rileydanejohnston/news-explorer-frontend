5041761d297d67a95f26bfc3fa24a298ed430631
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-28 13:41:16 -0600

    feat: add basic routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,23 @@
 import { React } from "react";
 import GlobalStyle from "../GlobalStyles/globalStyles";
+import { Route, Switch } from 'react-router-dom';
 import {
   Wrapper,
 } from './styledApp';
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
     <Wrapper>
       <GlobalStyle />
-      
+      <Switch>
+        <ProtectedRoute exact path='/saved-news' loggedIn={false}>
+          <h1>Testing saved-news route</h1>
+        </ProtectedRoute>
+        <Route exact path='/'>
+          <h1>Testing main route</h1>
+        </Route>
+      </Switch>
     </Wrapper>
   );
 }
