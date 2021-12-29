d1546288d338c512ee7cfad3d5134edaca49c6df
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-29 02:27:49 -0600

    fix: change App wrapper from h1 to div, remove fixed height

--- src/components/App/styledApp.js
+++ src/components/App/styledApp.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components/macro';
 
-export const Wrapper = styled.h1`
+export const Wrapper = styled.div`
   max-width: 1440px;
   min-width: 320px;
   margin: 0 auto;
-  height 150px;
 `;
