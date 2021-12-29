59ff4a1afdda2751e5611997ccf58a7916c9d636
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-28 23:42:27 -0600

    feat: add media queries to header comp

--- src/components/Header/styledHeader.js
+++ src/components/Header/styledHeader.js
@@ -8,6 +8,16 @@ export const Wrapper = styled.header`
   height: 80px;
   padding: 0 104px;
   box-shadow: 0px -1px 0px 0px #D1D2D6 inset;
+
+  @media( max-width: 1023px) {
+    height: 66px;
+    padding: 0 40px;
+  }
+
+  @media( max-width: 425px) {
+    height: 56px;
+    padding: 0 16px 0 20px;
+  }
 `;
 
 export const Logo = styled(Link)`
@@ -18,4 +28,8 @@ export const Logo = styled(Link)`
   font-weight: 700;
   line-height: 24px;
   letter-spacing: 0px;
+
+  @media( max-width: 425px ) {
+    font-size: 16px;
+  }
 `;
