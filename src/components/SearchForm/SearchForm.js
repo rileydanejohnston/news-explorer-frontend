eb84c9f807ad7721acb3d4f31b742cb890d0c174
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-30 13:49:01 -0600

    feat: make SearchForm controlled comp, prevent refresh on submit

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import {
   SearchContent,
   SearchWrapper,
@@ -10,13 +10,29 @@ import {
 } from './styledSearchForm'
 
 export default function SearchForm() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInput = (e) => {
+    setSearchQuery(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchQuery);
+  }
+
   return (
     <SearchWrapper>
       <SearchContent>
         <Title>What's going on in the world?</Title>
         <Subtitle>Find the latest news on any topic and save them in your personal account.</Subtitle>
-        <Form>
-          <Input placeholder='Enter topic' />
+        <Form onSubmit={handleSubmit}>
+          <Input 
+            value={searchQuery}
+            onChange={handleInput} 
+            placeholder='Enter topic'
+          />
           <Submit>Search</Submit>
         </Form>
       </SearchContent>
