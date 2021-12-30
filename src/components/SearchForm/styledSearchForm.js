b5b1fa9af05c0fa52143e9cf0fb7d89ec3fe3c9f
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-30 14:10:55 -0600

    feat: add media queries to SearchForm

--- src/components/SearchForm/styledSearchForm.js
+++ src/components/SearchForm/styledSearchForm.js
@@ -3,11 +3,28 @@ import styled from "styled-components/macro";
 export const SearchWrapper = styled.section`
   background: #6193EA;
   padding: 80px 0;
+
+  @media( max-width: 1023px ) {
+    padding: 38px 0 48px 0;
+  }
+
+  @media( max-width: 625px ) {
+    padding: 30px 0 32px 0;
+  }
 `;
 
 export const SearchContent = styled.div`
   width: 608px;
+  min-width: 288px;
   margin: 0 auto;
+
+  @media( max-width: 1023px ) {
+    width: 448px;
+  }
+
+  @media( max-width: 625px ) {
+    width: 288px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -18,6 +35,11 @@ export const Title = styled.h1`
   font-weight: 400;
   line-height: 64px;
   letter-spacing: 0px;
+
+  @media( max-width: 1023px ) {
+    font-size: 36px;
+    line-height: 40px;
+  }
 `;
 
 export const Subtitle = styled.h1`
@@ -29,6 +51,10 @@ export const Subtitle = styled.h1`
   letter-spacing: 0px;
   text-align: left;
   margin-top: 32px;
+
+  @media( max-width: 1023px ) {
+    margin-top: 16px;
+  }
 `;
 
 export const Form = styled.form`
@@ -36,6 +62,18 @@ export const Form = styled.form`
   height: 64px;
   margin-top: 88px;
   position: relative;
+
+  @media( max-width: 1023px ) {
+    margin-top: 48px;
+    height: 56px;
+  }
+
+  @media( max-width: 625px ) {
+    margin-top: 122px;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 128px;
+  }
 `;
 
 export const Input = styled.input`
@@ -43,7 +81,7 @@ export const Input = styled.input`
   border: 1px solid #D1D2D6;
   border-right: none;
   border-radius: 100px;
-  padding: 17px 24px;
+  padding: 21px 24px;
   position: relative;
 
   font-size: 16px;
@@ -52,6 +90,15 @@ export const Input = styled.input`
   line-height: 22px;
   letter-spacing: 0px;
   width: 100%;
+
+  @media( max-width: 1023px ) {
+    padding: 17px 24px;
+  }
+
+  @media( max-width: 625px ) {
+    padding: 16px;
+    width: auto;
+  }
 `;
 
 export const Submit = styled.button`
@@ -72,4 +119,15 @@ export const Submit = styled.button`
   line-height: 24px;
   letter-spacing: 0px;
   text-align: center;
+
+  @media( max-width: 1023px ) {
+    width: 160px;
+    padding: 16px 52px;
+  }
+
+  @media( max-width: 625px ) {
+    padding: 16px 116px;
+    position: static;
+    width: 100%;
+  }
 `;
