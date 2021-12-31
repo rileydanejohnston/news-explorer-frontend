4a6f09b71a9e57ac73c02d599577d10d7c116727
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-31 00:18:18 -0600

    fix: move Card to it's own component. NewsCardList remains a component

---
+++ src/components/Card/styledCard.js
@@ -0,0 +1,63 @@
+import styled from "styled-components/macro";
+
+
+export const CardWrapper = styled.li`
+  width: 400px;
+  border-radius: 20px;
+  overflow: hidden;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 272px;
+`;
+
+export const InfoWrapper = styled.div`
+  padding: 20px 24px 24px;
+  background: #FFF;
+`;
+
+export const Date = styled.p`
+  font-family: 'Source Sans Pro';
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0px;
+
+  color: #B6BCBF;
+`;
+
+export const Title = styled.h3`
+  font-family: 'Roboto Slab';
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: 0px;
+
+  margin-top: 12px;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0px;
+
+  margin-top: 46px;
+`;
+
+export const NewsSource = styled.p`
+  font-family: 'Roboto Slab';
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: 0.4000000059604645px;
+
+  margin-top: 40px;
+  text-transform: uppercase;
+  color: #B6BCBF;
+`;
