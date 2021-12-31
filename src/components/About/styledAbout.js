a181e0a131bb17c7825072b8dee482e4ae5b8145
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-30 21:47:55 -0600

    feat: add media queries to About section

--- src/components/About/styledAbout.js
+++ src/components/About/styledAbout.js
@@ -3,27 +3,58 @@ import styled from "styled-components/macro";
 export const AboutWrapper = styled.section`
   background: #FFF;
   padding: 80px 216px 80px 104px;
+
+  @media(max-width: 1439px) {
+    padding: 40px 0 61px;
+  }
+  @media(max-width: 625px) {
+    padding: 32px 0 128px;
+  }
 `;
 
 export const AboutContent = styled.div`
-  border: 1px solid blue;
-  height: 100%;
-
   display: flex;
-  justify-content: space-between;
   grid-gap: 56px;
   width: 1120px;
   margin: 0 auto;
+
+  @media(max-width: 1439px) {
+    grid-gap: 32px;
+    width: 920px;
+  }
+  @media(max-width: 1023px) {
+    width: 688px;
+  }
+  @media(max-width: 767px) {
+    flex-direction: column;
+    align-items: center;
+    width: 288px;
+    grid-gap: 24px;
+  }
 `;
 
 export const AuthorImg = styled.img`
   width: 464px;
   height: 464px;
   border-radius: 1000px;
+
+  @media(max-width: 1023px) {
+    width: 232px;
+    height: 232px;
+  }
+  @media(max-width: 625px) {
+    width: 272px;
+    height: 272px;
+  }
 `;
 
 export const TitlesWrapper = styled.div`
   padding-top: 51px;
+
+  @media(max-width: 1439px) {
+    padding-top: 40px;
+    width: 424px;
+  }
 `;
 
 export const Title = styled.h2`
@@ -34,7 +65,10 @@ export const Title = styled.h2`
   line-height: 46px;
   letter-spacing: 0px;
 
-
+  @media(max-width: 1439px) {
+    font-size: 30px;
+    line-height: 40px;
+  }
 `;
 
 export const ParagraphWrapper = styled.ul`
@@ -42,6 +76,10 @@ export const ParagraphWrapper = styled.ul`
   flex-direction: column;
   grid-gap: 16px;
   margin-top: 24px;
+
+  @media(max-width: 1439px) {
+    margin-top: 16px;
+  }
 `;
 
 export const Paragraph = styled.li`
