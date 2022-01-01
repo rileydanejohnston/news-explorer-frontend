93b5c75552cac638e9a104892d786e806483799e
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-31 19:05:16 -0600

    feat: add basic markup for search results

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { DevelopmentWrapper, CardList } from './styledNewsCardList'
+import { DevelopmentWrapper, CardList, Title, ShowMoreButton } from './styledNewsCardList'
 import Card from '../Card/Card';
 
 export default function NewsCardList({ loggedIn }) {
   return (
     <DevelopmentWrapper>
+      <Title>Search results</Title>
       <CardList>
         <Card loggedIn={loggedIn} />
         <Card loggedIn={loggedIn} />
         <Card loggedIn={loggedIn} />
         <Card loggedIn={loggedIn} />
       </CardList>
+      <ShowMoreButton>Show more</ShowMoreButton>
     </DevelopmentWrapper>
   )
 }
--- src/components/NewsCardList/styledNewsCardList.js
+++ src/components/NewsCardList/styledNewsCardList.js
@@ -2,6 +2,20 @@ import styled from "styled-components/macro";
 
 export const DevelopmentWrapper = styled.section`
   background: #F5F6F7;
+  padding: 80px 104px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h2`
+  font-family: 'Roboto Slab';
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 46px;
+  letter-spacing: 0px;
+
+  margin-bottom: 64px;
 `;
 
 export const CardList = styled.ul`
@@ -33,6 +47,26 @@ export const CardList = styled.ul`
     display: flex;
     flex-direction: column;
     grid-gap: 10px;
+  }
+`;
+
+export const ShowMoreButton = styled.button`
+  width: 288px;
+  height: 64px;
+  background: #E8E8E8;
+  border-radius: 80px;
+  border: none;
+  margin: 64px auto 0 auto;
+
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0px;
+  text-align: center;
+
+  &:hover {
+    cursor: pointer;
   }
 `;
 
